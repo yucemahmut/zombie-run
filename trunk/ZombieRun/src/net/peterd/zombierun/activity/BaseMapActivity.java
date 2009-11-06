8fff41f7ff6e272c01f3a0d69b99bbed5bdeab4f
peterJdolan <peterJdolan@1691cc1e-3247-11de-9d05-31133e6853b1>
2009-11-06 00:23:43 +0000

    Refinement

--- trunk/ZombieRun/src/net/peterd/zombierun/activity/BaseMapActivity.java
+++ trunk/ZombieRun/src/net/peterd/zombierun/activity/BaseMapActivity.java
@@ -5,8 +5,6 @@ package net.peterd.zombierun.activity;
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 import com.google.android.maps.MapActivity;
 
-import android.util.Log;
-
 public class BaseMapActivity extends MapActivity {
 
   protected GoogleAnalyticsTracker tracker;
@@ -14,11 +12,7 @@ public class BaseMapActivity extends MapActivity {
   @Override
   public void onStart() {
     super.onStart();
-    tracker = GoogleAnalyticsTracker.getInstance();
-    tracker.start("UA-214814-13", this);
-    String pageView = "/action/" + this.getClass().getSimpleName();
-    Log.d("ZombieRun.BaseMapActivity", pageView);
-    tracker.trackPageView(pageView);
+    BaseActivity.logToAnalytics(this);
   }
   
   @Override
