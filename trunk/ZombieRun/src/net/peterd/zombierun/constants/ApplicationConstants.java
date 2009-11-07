824bbebcea6f6f4f1a30f02a6787ed1171ae3732
peterJdolan <peterJdolan@1691cc1e-3247-11de-9d05-31133e6853b1>
2009-11-07 19:12:21 +0000

    A few more attempts at optimization

--- trunk/ZombieRun/src/net/peterd/zombierun/constants/ApplicationConstants.java
+++ trunk/ZombieRun/src/net/peterd/zombierun/constants/ApplicationConstants.java
@@ -4,18 +4,8 @@ public class ApplicationConstants {
 
   private static final boolean developing = true;
   
-  private static final String develMapsApiKey = "0gTCqChu4DUp2u2dMhUspP8EJlh8YeMUjO3FYqQ";
-
-  // This is not filled in because the key could be stolen from the public
-  // source code repository.
-  private static final String releaseMapsApiKey = "--- filled in by the project manager before each release ---";
-
-  public static String getMapsApiKey() {
-    return developing ? develMapsApiKey : releaseMapsApiKey;
-  }
-  
   public static boolean loggingEnabled() {
-    return developing;
+    return developing && false;
   }
   
   public static boolean testing() {
