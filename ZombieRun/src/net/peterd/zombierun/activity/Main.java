e586ebe6994fb7dce0a56a59ebdc2411ccbb9b95
peterJdolan <peterJdolan@1691cc1e-3247-11de-9d05-31133e6853b1>
2010-04-19 17:41:26 +0000

    integrate the web version into the main game

--- ZombieRun/src/net/peterd/zombierun/activity/Main.java
+++ ZombieRun/src/net/peterd/zombierun/activity/Main.java
@@ -1,18 +1,13 @@
 package net.peterd.zombierun.activity;
 
-import net.peterd.zombierun.constants.ApplicationConstants;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class Main extends BaseActivity {
-  
+
   @Override
   public void onCreate(Bundle state) {
     super.onCreate(state);
-    if (ApplicationConstants.multiplayerEnabled) {
-      startActivity(new Intent(this, MainMenuWithMultiplayerEnabled.class));
-    } else {
-      startActivity(new Intent(this, MainMenuWithMultiplayerDisabled.class));
-    }
+    startActivity(new Intent(this, MainMenuWithMultiplayerDisabled.class));
   }
 }
--- ZombieRun/src/net/peterd/zombierun/activity/MainMenuWithMultiplayerDisabled.java
+++ ZombieRun/src/net/peterd/zombierun/activity/MainMenuWithMultiplayerDisabled.java
@@ -2,18 +2,19 @@ package net.peterd.zombierun.activity;
 
 import net.peterd.zombierun.R;
 import net.peterd.zombierun.game.GameSettings;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainMenuWithMultiplayerDisabled extends MainMenu {
-  
+
   private void startGame() {
     Bundle bundle = new Bundle();
     GameSettings settings = Util.handleGameSettings(this, false);
     settings.toBundle(bundle);
-    
+
     Intent startGameIntent = new Intent(this, StartGame.class);
     startGameIntent.putExtras(bundle);
     startActivity(startGameIntent);
@@ -22,6 +23,14 @@ public class MainMenuWithMultiplayerDisabled extends MainMenu {
   @Override
   protected void setupMainLayout() {
     setContentView(R.layout.main_multiplayer_disabled);
+
+    final Activity activity = this;
+    ((Button) findViewById(R.id.button_start_multiplayer)).setOnClickListener(
+        new View.OnClickListener() {
+          public void onClick(View v) {
+            startActivity(new Intent(activity, WebGame.class));
+          }
+        });
 
     ((Button) findViewById(R.id.button_start_singleplayer)).setOnClickListener(
         new View.OnClickListener() {
@@ -38,7 +47,7 @@ public class MainMenuWithMultiplayerDisabled extends MainMenu {
         public void onClick(View v) {
           startActivity(showHelpIntent);
         }
-      });    
+      });
 
     Util.configureAds(this);
   }
