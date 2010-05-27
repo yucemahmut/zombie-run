7b894fd3db65cc0f3471758d230343673688ee36
peterJdolan <peterJdolan@1691cc1e-3247-11de-9d05-31133e6853b1>
2010-05-27 03:47:30 +0000

    upgrade to the latest version of google ads, set some new ad spec parameters

--- trunk/ZombieRun/src/net/peterd/zombierun/activity/Util.java
+++ trunk/ZombieRun/src/net/peterd/zombierun/activity/Util.java
@@ -1,11 +1,5 @@
 package net.peterd.zombierun.activity;
 
-import com.google.ads.AdSenseSpec;
-import com.google.ads.AdViewListener;
-import com.google.ads.GoogleAdView;
-import com.google.ads.AdSenseSpec.AdType;
-import com.google.android.apps.analytics.GoogleAnalyticsTracker;
-
 import net.peterd.zombierun.R;
 import net.peterd.zombierun.constants.ApplicationConstants;
 import net.peterd.zombierun.game.GameSettings;
@@ -14,6 +8,12 @@ import net.peterd.zombierun.util.Log;
 import android.app.Activity;
 import android.os.Handler;
 import android.widget.Spinner;
+
+import com.google.ads.AdSenseSpec;
+import com.google.ads.AdViewListener;
+import com.google.ads.GoogleAdView;
+import com.google.ads.AdSenseSpec.AdType;
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 
 public class Util {
 
@@ -25,7 +25,7 @@ public class Util {
         activity.getResources().getStringArray(
             R.array.zombie_speeds_average_speed_mph)[indexOfSpeed]);
     double zombieSpeed = GeoCalculationUtil.milesPerHourToMetersPerSecond(zombieSpeedMph);
-    
+
     Spinner zombieCountSpinner = (Spinner) activity.findViewById(R.id.spinner_zombie_count);
     int indexOfDensity = zombieCountSpinner.getSelectedItemPosition();
     double averageDistanceBetweenZombiesMeters = Double.parseDouble(
@@ -34,38 +34,38 @@ public class Util {
     double zombieDensity =
         GeoCalculationUtil.itemPerDistanceToItemsPerSquareKilometer(
             averageDistanceBetweenZombiesMeters);
-    
+
     return new GameSettings(zombieDensity, zombieSpeed, startingMultiplayerGame);
   }
-  
+
   public static void configureAds(final Activity activity) {
     if (Log.loggingEnabled()) {
-      Log.d("ZombieRun.Util", 
+      Log.d("ZombieRun.Util",
           "Adsense Ads Keywords: " + activity.getString(R.string.adsense_keywords));
     }
-    final GoogleAdView adView = 
+    final GoogleAdView adView =
         (GoogleAdView) activity.findViewById(R.id.adview);
     final GoogleAnalyticsTracker tracker =
         BaseActivity.getAnalyticsTracker(activity);
     AdViewListener listener = new AdViewListener() {
           public void onStartFetchAd() {
             tracker.trackEvent("ads",
-                "start_fetch", 
-                activity.getClass().getSimpleName(), 
+                "start_fetch",
+                activity.getClass().getSimpleName(),
                 1);
             tracker.dispatch();
           }
           public void onFinishFetchAd() {
             tracker.trackEvent("ads",
-                "finish_fetch", 
-                activity.getClass().getSimpleName(), 
+                "finish_fetch",
+                activity.getClass().getSimpleName(),
                 1);
             tracker.dispatch();
           }
           public void onClickAd() {
             tracker.trackEvent("ads",
-                "click", 
-                activity.getClass().getSimpleName(), 
+                "click",
+                activity.getClass().getSimpleName(),
                 1);
             tracker.dispatch();
           }
@@ -74,7 +74,7 @@ public class Util {
 
     new Handler().post(new Runnable() {
         public void run() {
-          AdSenseSpec adSenseSpec = 
+          AdSenseSpec adSenseSpec =
               new AdSenseSpec(activity.getString(R.string.adsense_pub_id))
                   .setCompanyName(
                       activity.getString(R.string.adsense_company_name))
@@ -83,7 +83,10 @@ public class Util {
                   .setChannel(activity.getString(R.string.adsense_channel))
                   .setAdType(AdType.TEXT_IMAGE)
                   .setWebEquivalentUrl(activity.getString(R.string.about_url))
-                  .setAdTestEnabled(ApplicationConstants.testing());
+                  .setAdTestEnabled(ApplicationConstants.testing())
+                  .setExpandDirection(AdSenseSpec.ExpandDirection.TOP)
+                  .setAdType(AdSenseSpec.AdType.TEXT_IMAGE)
+                  .setAdFormat(AdSenseSpec.AdFormat.FORMAT_320x50);
 
           // Fetch Google ad.
           // PLEASE DO NOT CLICK ON THE AD UNLESS YOU ARE IN TEST MODE.
